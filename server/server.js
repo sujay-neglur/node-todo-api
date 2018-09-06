48bcafe91cd2515b36e0d645b7d8284396dd388c
sujayneglur123 <sujayneglur123@gmail.com>
2018-09-06 01:23:11 -0400

    test scripts added

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
-var express= require('express');
-var bodyParser= require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var {mongoose}= require('./db/mongoose');
-var {Todo}= require('./models/todo');
-var {User}= require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
-var app =express();
+var app = express();
 app.use(bodyParser.json()); //middleware
 
 app.post('/todos', (request, response) => {
     console.log(request.body);
-    var todo= new Todo({
-        text:request.body.text
+    var todo = new Todo({
+        text: request.body.text
     });
     todo.save().then(doc => response.send(doc), (err) => response.status(400).send(err));
 });
 
 app.listen(3000, () => console.log('Started on port 3000'));
 
+module.exports={
+    app
+}
